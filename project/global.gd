5c1d39ce488fe086cd57944acd3165fb2d1922b0
wonkanese <northkoreanwillywonka@gmail.com>
2024-03-19 17:05:50 -0600

    Enemies

--- project/global.gd
+++ project/global.gd
@@ -2,3 +2,9 @@ extends Node
 
 var sel_costume = 1
 var sel_hair = 1
+
+var enemy_stats = {
+	"Ghost" : {"Health" : 4, "Speed" : 650},
+	"Enemy" : {"Health" : 3, "Speed" : 400},
+	"Slime" : {"Health" : 1, "Speed" : 300}
+}
